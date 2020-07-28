67a62fd3576eab8fca8ac67afbdc800682ef9914
sonu <anubhavk@anubkuma.ap.qualcomm.com>
2020-07-28 12:42:33 +0530

    python QC train intialization

---
+++ qctrain/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python python
+#test.py
+#anubhav
+
+print ("hello world!")
+name = input("Please enter your name")
+if (len(name) > 0):
+	print ("hello " + name)
+else:
+	print ("not a valid name")
+
